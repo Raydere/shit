5a46e6edc2ec8f2fe542517b0ff9a45c41e9257c
Raydere <frozenray13@gmail.com>
2013-01-14 03:55:58 -0800

    awb jechts

---
+++ ruby/chapter3/objects.rb
@@ -0,0 +1,43 @@
+class IIDXPlayer
+  attr_reader :id, :djName, :danRank
+  def initialize(id, djName, danRank)
+    @id = id
+    @djName = djName
+    @danRank = danRank
+  end
+  def promote(newDanRank)
+    @danRank = newDanRank
+  end
+  def danPrintableFormat
+    danString = ""
+    if @danRank > 0
+      danString = "#{@danRank}-dan"
+    elsif @danRank < 0
+      danString = "#{-@danRank}-kyu"
+    else
+      danString = "--"
+    end
+    return danString
+  end
+  def to_s    
+    "ID: #{@id}, DJ Name: #{@djName}, Rank: #{danPrintableFormat}"
+  end
+end
+
+reimu = IIDXPlayer.new(43983017, "REIMU", 6)
+p reimu
+
+johoto = IIDXPlayer.new(40653091, "JK.356", 6)
+p johoto
+
+koishi = IIDXPlayer.new(49330652, "KOISHI", 6)
+p koishi
+
+sakuya = IIDXPlayer.new(41935859, "SAKUYA", -5)
+p sakuya
+
+puts "holy shit #{reimu.djName} cleared the Safari"
+reimu.promote(7)
+puts "#{reimu.djName} is now #{reimu.danPrintableFormat}"
+
+puts reimu # reimu no
